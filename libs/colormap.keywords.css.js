2213b1dba580afa1f5b7fc2c37f68a9395c1dc0f
FabioVergani <fabio.vergani.1975@gmail.com>
2015-08-19 15:45:13 +0200

    Update colormap.keywords.css.js

--- libs/colormap.keywords.css.js
+++ libs/colormap.keywords.css.js
@@ -9,11 +9,7 @@ var colormap={
 	}
  }
 };
-
 //-----------------------------------------------------------------------------
-
-//fullhex = /^#([0-9a-f]{6})$/i;
-
 function ShrinkHex(s){
  var m=s,a=m[0],b=m[2],c=m[4];
  return (a===m[1] && b===m[3] && c===m[5])?[m[0],m[2],m[4]].join(''):m;
@@ -23,16 +19,24 @@ var p, o=colormap.keywords.css, result=[];
 for(p in o){result[result.length]=[p,':\'',ShrinkHex(o[p]),'\''].join('');};
 console.log(result.join());
 */
-var colormap={
- keywords:{
-	css:{//short:
+
+/*
+var p, o=colormap.keywords.css, result=[];
+for(p in o){if(p.length<o[p].length){result[result.length]=p;};};
+console.log('color-keywords shorter than color-hex',result.join());//olive,green,navy,teal,gray
+*/
+var colors={
+ pattern={
+	keywords_shorter_than_hex:'olive|green|navy|teal|gray',
+ },
+ map={
+	keyword:{
+	 css:{//short:
 		maroon:'800000',red:'f00',orange:'ffa500',yellow:'ff0',olive:'808000',
 		purple:'800080',fuchsia:'f0f',white:'fff',lime:'0f0',green:'008000',
 		navy:'000080',blue:'00f',aqua:'0ff',teal:'008080',
 		black:'000',silver:'c0c0c0',gray:'808080'
+	 }
 	}
- }
+ };
 };
-
-
-
